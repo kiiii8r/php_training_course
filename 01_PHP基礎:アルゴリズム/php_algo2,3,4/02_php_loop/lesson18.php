224594fcab7b64c609599d63758a46cdc0f9c09d
kiiii8r <gkii.8r@gmail.com>
2021-06-15 17:55:21 +0900

    add

---
+++ 01_PHP基礎:アルゴリズム/php_algo2,3,4/02_php_loop/lesson18.php
@@ -0,0 +1,88 @@
+﻿<?php 
+// 以下配列の中身をfor文を使用して表示してください。
+// 表示はHTMLの<table>タグを使用すること
+
+/**
+ * 表示イメージ
+ *  _________________________
+ *  |_____|_c1|_c2|_c3|横合計|
+ *  |___r1|_10|__5|_20|___35|
+ *  |___r2|__7|__8|_12|___27|
+ *  |___r3|_25|__9|130|__164|
+ *  |縦合計|_42|_22|162|__226|
+ *  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+ */
+
+$arr = [
+    'r1' => ['c1' => 10, 'c2' => 5, 'c3' => 20],
+    'r2' => ['c1' => 7, 'c2' => 8, 'c3' => 12],
+    'r3' => ['c1' => 25, 'c2' => 9, 'c3' => 130]
+];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>テーブル表示</title>
+  <style>
+  table {
+    border: 1px solid #000;
+    border-collapse: collapse;
+    text-align: right;
+  }
+
+  th,
+  td {
+    border: 1px solid #000;
+  }
+  </style>
+</head>
+
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+        <?php foreach($arr as $key => $val): ?>
+        <?php foreach($val as $cel => $data): ?>
+        <th><?= $cel ?></th>
+        <?php endforeach; ?>
+        <?php break; ?>
+        <?php endforeach; ?>
+        <th>横合計</th>
+      </tr>
+
+      <thead>
+
+      <tbody>
+        <?php $rec_total = ['c1'=>0,'c2'=>0,'c3'=>0]; ?>
+
+        <?php foreach($arr as $key): ?>
+        <tr>
+          <?php $col_total = 0 ?>
+          <td><?= key($arr) ?></td>
+
+          <?php foreach($key as $val => $data): ?>
+          <td><?= $data ?></td>
+          <?php $col_total += $data; ?>
+          <?php $rec_total[$val] += $data ?>
+          <?php endforeach; ?>
+          <td><?= $col_total ?></td>
+
+        </tr>
+        <?php next($arr); ?>
+        <?php endforeach; ?>
+        <th>縦合計</th>
+        <?php $col_total = 0 ?>
+        <?php foreach($rec_total as $data): ?>
+        <td><?= $data ?></td>
+        <?php $col_total += $data; ?>
+        <?php endforeach; ?>
+        <td><?= $col_total  ?></td>
+      </tbody>
+
+  </table>
+</body>
+
+</html>
---
+++ 01_PHP基礎:アルゴリズム/php_algo2,3,4/03_php_algo/algorithm2.php
@@ -0,0 +1,73 @@
+<?php
+// ＜アルゴリズムの注意点＞
+// アルゴリズムではこれまでのように調べる力ではなく物事を論理的に考える力が必要です。
+// 検索して答えを探して解いても考える力には繋がりません。
+// まずは検索に頼らずに自分で処理手順を考えてみましょう。
+
+
+// 以下は自動販売機のお釣りを計算するプログラムです。
+// 150円のお茶を購入した際のお釣りを計算して表示してください。
+// 計算内容は関数に記述し、関数を呼び出して結果表示すること
+// $yen と $product の金額を変更して計算が合っているか検証を行うこと
+
+// 表示例1）
+// 10000円札で購入した場合、
+// 10000円札x0枚、5000円札x1枚、1000円札x4枚、500円玉x1枚、100円玉x3枚、50円玉x1枚、10円玉x0枚、5円玉x0枚、1円玉x0枚
+
+// 表示例2）
+// 100円玉で購入した場合、
+// 50円足りません。
+
+$yen = 10000;   // 購入金額
+$product = 150; // 商品金額
+
+// 入金金額の過不足
+function calc($yen, $product) {
+    $moneys = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1];
+    $amount = $yen - $product;
+
+    if ($amount >= 0){
+        foreach($moneys as $money):
+            $amount = quantity($amount, $money);
+        endforeach;
+    } else {    
+        $shortage = $yen - $product;
+        echo $shortage . "円足りません。";
+    }
+}
+
+// お釣りの計算
+function quantity($amount, $money){
+    $sum = 0;
+    if($amount >= $money) {
+        for($i = 0 ; $amount >= $money; $i++){
+            $amount -= $money;
+            $sum++;
+        }
+    }
+    
+    if($money >= 1000){
+        echo $money . '円札x' . $sum . '枚、';
+        return $amount;
+    } else {
+        echo $money . '円玉x' . $sum . '枚、';
+        return $amount;
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>お釣り</title>
+</head>
+
+<body>
+  <section>
+    <?php calc($yen, $product) ?>
+  </section>
+</body>
+
+</html>
