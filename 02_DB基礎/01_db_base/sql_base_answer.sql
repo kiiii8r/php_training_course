b6b597a3c8ae29712461f5b430d137dc33c4f152
kiiii8r <gkii.8r@gmail.com>
2021-06-27 23:11:55 +0900

    問２９

--- 02_DB基礎/01_db_base/sql_base_answer.sql
+++ 02_DB基礎/01_db_base/sql_base_answer.sql
@@ -133,3 +133,8 @@ USE cafe;
 -- 問２８
 -- SELECT AVG(height) AS '平均身長', AVG(weight) AS '平均体重'
 -- FROM players;
+
+-- 問２９
+-- SELECT COUNT(id) AS '日本のゴール数'
+-- FROM goals
+-- WHERE player_id BETWEEN 714 AND 736;
