f71f162300e8afe2ba5d298d17e0b129ce139bfd
kiiii8r <gkii.8r@gmail.com>
2021-06-27 23:11:28 +0900

    問２７

--- 02_DB基礎/01_db_base/sql_base_answer.sql
+++ 02_DB基礎/01_db_base/sql_base_answer.sql
@@ -121,3 +121,11 @@ USE cafe;
 -- 問２６
 -- SELECT IFNULL(player_id, 9999) AS player_id, goal_time
 -- FROM goals;
+
+-- 問２７
+-- SELECT 
+--   CASE WHEN player_id IS NULL THEN 9999 
+--   ELSE player_id END 
+--   AS player_id, 
+--   goal_time
+-- FROM goals;
