cfaea0b90fab77e8565349a3ca9a980e4e7da18f
kiiii8r <gkii.8r@gmail.com>
2021-06-30 22:37:35 +0900

    Q3

--- 02_DB基礎/02_db_advanced/sql_adv_answer.sql
+++ 02_DB基礎/02_db_advanced/sql_adv_answer.sql
@@ -9,3 +9,11 @@ USE cafe;
 -- SELECT AVG(height) AS '平均身長', AVG(weight) AS '平均体重'
 -- FROM players
 -- WHERE position = 'GK';
+
+-- 問３
+-- SELECT c.name AS '国名', AVG(p.height) AS '平均身長'
+-- FROM countries AS c
+-- JOIN players AS p
+-- ON c.id = p.country_id
+-- GROUP BY c.name
+-- ORDER BY AVG(p.height) DESC;
