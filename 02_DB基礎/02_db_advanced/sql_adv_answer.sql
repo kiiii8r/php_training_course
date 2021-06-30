5602767ce0778018f898f0fb7f59b60e601b04e9
kiiii8r <gkii.8r@gmail.com>
2021-06-30 22:37:26 +0900

    Q2

--- 02_DB基礎/02_db_advanced/sql_adv_answer.sql
+++ 02_DB基礎/02_db_advanced/sql_adv_answer.sql
@@ -4,3 +4,8 @@ USE cafe;
 -- SELECT group_name AS 'グループ', MIN(ranking) AS 'ランキング最上位', MAX(ranking) AS 'ランキング最下位'
 -- FROM countries
 -- GROUP BY group_name;
+
+-- 問２
+-- SELECT AVG(height) AS '平均身長', AVG(weight) AS '平均体重'
+-- FROM players
+-- WHERE position = 'GK';
